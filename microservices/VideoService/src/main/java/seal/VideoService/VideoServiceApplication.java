df66070f02c24b37d44f0e881b7256206444dc4f
gammyp <gamebep@gmail.com>
2018-12-22 23:15:19 +0700

    [ADD] Add config discovery modlues

--- microservices/VideoService/src/main/java/seal/VideoService/VideoServiceApplication.java
+++ microservices/VideoService/src/main/java/seal/VideoService/VideoServiceApplication.java
@@ -2,10 +2,12 @@ package seal.VideoService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableDiscoveryClient
 public class VideoServiceApplication {
 
     public static void main(String[] args) {
